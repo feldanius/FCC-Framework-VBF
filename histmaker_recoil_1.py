6af1543316d1c3c8a3841a133fa93a4fe127923c
feldanius <43442037+feldanius@users.noreply.github.com>
2024-09-08 07:31:06 -0600

    Update histmaker_recoil_1.py

--- histmaker_recoil_1.py
+++ histmaker_recoil_1.py
@@ -1,29 +1,75 @@
 import ROOT
 import os
 
-# Lista de procesos (obligatoria)
+# list of processes (mandatory)
 processList = {
-    'p8_ee_WW_mumu_ecm240':    {'fraction': 1, 'crossSection': 0.25792}, 
-    'p8_ee_ZZ_mumubb_ecm240':  {'fraction': 1, 'crossSection': 2 * 1.35899 * 0.034 * 0.152},
-    'p8_ee_ZH_Zmumu_ecm240':   {'fraction': 1, 'crossSection': 0.201868 * 0.034},
+    'wzp6_ee_eeH_ecm365': {
+        'files': [
+    "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_eeH_ecm365/events_018107429.root",
+    "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_eeH_ecm365/events_104473316.root",
+    "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_eeH_ecm365/events_117039771.root",
+    "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_eeH_ecm365/events_130128294.root",
+    "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_eeH_ecm365/events_173768499.root",
+    "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_eeH_ecm365/events_047118042.root",
+    "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_eeH_ecm365/events_107849662.root",
+    "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_eeH_ecm365/events_118636096.root",
+    "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_eeH_ecm365/events_153113508.root",
+    "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_eeH_ecm365/events_182514790.root"
+        ],
+        'crossSection': 0.00739
+    },
+    'wzp6_ee_mumuH_ecm365': {
+        'files': [
+            "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_023261681.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_034927544.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_058572739.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_073291643.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_092281869.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_189404834.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_033570460.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_036028207.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_068513877.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_075232335.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_183713990.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_197337122.root"
+        ],
+        'crossSection': 0.004185
+    },
+    'wzp6_ee_tautauH_ecm365': {
+        'files': [
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_tautauH_ecm365/events_015660911.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_tautauH_ecm365/events_058437289.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_tautauH_ecm365/events_094523955.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_tautauH_ecm365/events_160031364.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_tautauH_ecm365/events_175873169.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_tautauH_ecm365/events_192276123.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_tautauH_ecm365/events_043999218.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_tautauH_ecm365/events_090181769.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_tautauH_ecm365/events_116046380.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_tautauH_ecm365/events_162671919.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_tautauH_ecm365/events_186940475.root"
+
+        ],
+        'crossSection': 0.004172
+    }
 }
 
-# Ruta al diccionario de secciones transversales (obligatoria)
+# Link to the dictonary that contains all the cross section informations etc... (mandatory)
 procDict = "FCCee_procDict_winter2023_IDEA.json"
 
-# Incluye rutas adicionales a los archivos de encabezado personalizados
+# additional/custom C++ functions, defined in header files (optional)
 includePaths = ["functions_1.h"]
 
-# Define el input dir (opcional)
-inputDir = "./localSamples/"
+# Define the input dir (optional)
+# inputDir = "./localSamples/"
 
-# Directorio de salida opcional, por defecto es el directorio local de ejecución
-outputDir = "./outputs/histmaker/recoil/"
+#Optional: output directory, default is local running directory
+outputDir = "./outputs/histmaker_1/recoil_1/"
 
-# Número de CPUs opcional, por defecto es 4, -1 usa todos los núcleos disponibles
+# optional: ncpus, default is 4, -1 uses all cores available
 nCPUS = -1
 
-# Escala los histogramas con la sección transversal y la luminosidad integrada
+# scale the histograms with the cross-section and integrated luminosity
 doScale = True
 intLumi = 2400000  # 2.4 /ab
 
@@ -53,15 +99,21 @@ def compile_headers(includePaths):
 # Compila las cabeceras personalizadas antes de realizar el análisis
 compile_headers(includePaths)
 
-# Función build_graph que contiene la lógica de análisis, cortes y histogramas (obligatorio)
-def build_graph(df, dataset):
+# Función para cargar archivos
+def load_files(process_name):
+    chain = ROOT.TChain("events")  # Reemplaza "treeName" con el nombre correcto del TTree
+    for file in processList[process_name]['files']:
+        chain.Add(file)
+    return chain
 
-    results = []
-    df = df.Define("weight", "1.0")
-    
+# Función build_graph que contiene la lógica de análisis, cortes y histogramas (obligatorio)
+def build_graph(process_name):
+    chain = load_files(process_name)
+    df = ROOT.RDataFrame(chain)
+
     # Escala de peso por sección transversal y luminosidad
     if doScale:
-        crossSection = processList[dataset]['crossSection']
+        crossSection = processList[process_name]['crossSection']
         weightScale = crossSection * intLumi
         df = df.Define("weight", f"1.0 * {weightScale}")
 
@@ -104,6 +156,7 @@ def build_graph(df, dataset):
     df = df.Define("jets_no_leptons", "FCCAnalyses::ZHfunctions::removeJetsWithLeptons(Jets, muons, electrons)")
 
     # Histograma base, antes de cualquier selección de corte (guardar con _cut0)
+    results = []
     results.append(df.Histo1D(("muons_p_cut0", "", *bins_p_mu), "muons_p"))
     results.append(df.Histo1D(("muons_theta_cut0", "", *bins_theta), "muons_theta"))
     results.append(df.Histo1D(("muons_phi_cut0", "", *bins_phi), "muons_phi"))
@@ -131,18 +184,66 @@ def build_graph(df, dataset):
     df = df.Define("cut2", "2")
     results.append(df.Histo1D(("cutFlow", "", *bins_count), "cut2"))
 
-    # Construye la resonancia de Higgs
+    #########
+    ### CUT 3: Higgs mass window
+    #########
     df = df.Define("higgsbuilder_result", "FCCAnalyses::ZHfunctions::resonanceBuilder_mass_recoil(125, 125, 0.4, 240, false)(muons, MCRecoAssociations0, MCRecoAssociations1, ReconstructedParticles, Particle, Particle0, Particle1)")
     df = df.Define("higgs", "Vec_rp{higgsbuilder_result[0]}") # el Higgs
     df = df.Define("higgs_muons", "Vec_rp{higgsbuilder_result[1],higgsbuilder_result[2]}") # los leptones 
     df = df.Define("higgs_m", "FCCAnalyses::ReconstructedParticle::get_mass(higgs)")
     df = df.Define("higgs_p", "FCCAnalyses::ReconstructedParticle::get_p(higgs)")
     df = df.Define("higgs_recoil_m", "FCCAnalyses::ReconstructedParticle::recoilBuilder(240)(higgs)")
+    
+    df = df.Filter("higgs_m > 120 && higgs_m < 130")
+    df = df.Define("cut3", "3")
+    results.append(df.Histo1D(("cutFlow", "", *bins_count), "cut3"))
 
-    # Histogramas después del corte 4
-    results.append(df.Histo1D(("higgs_m_cut4", "", *bins_m_ll), "higgs_m"))
-    results.append(df.Histo1D(("higgs_p_cut4", "", *bins_p_ll), "higgs_p"))
-    results.append(df.Histo1D(("higgs_recoil_m_cut4", "", *bins_recoil), "higgs_recoil_m"))
-    results.append(df.Histo1D(("higgs_muons_p_cut4", "", *bins_p_mu), "higgs_muons_p"))
+    #########
+    ### CUT 4: Higgs momentum
+    #########
+    df = df.Filter("higgs_p > 20 && higgs_p < 70")
+    df = df.Define("cut4", "4")
+    results.append(df.Histo1D(("cutFlow", "", *bins_count), "cut4"))
 
-    return results
+    #########
+    ### CUT 5: cosThetaMiss
+    #########
+    df = df.Define("missingEnergy", "FCCAnalyses::ZHfunctions::missingEnergy(240., ReconstructedParticles)")
+    df = df.Define("cosTheta_miss", "FCCAnalyses::ZHfunctions::get_cosTheta_miss(MissingET)")
+    results.append(df.Histo1D(("cosThetaMiss_cut4", "", *bins_cosThetaMiss), "cosTheta_miss"))  # Plot it before the cut
+
+    df = df.Filter("cosTheta_miss < 0.98")
+    df = df.Define("cut5", "5")
+    results.append(df.Histo1D(("cutFlow", "", *bins_count), "cut5"))
+
+    #########
+    ### CUT 6: recoil mass window
+    #########
+    df = df.Filter("higgs_recoil_m < 140 && higgs_recoil_m > 120")
+    df = df.Define("cut6", "6")
+    results.append(df.Histo1D(("cutFlow", "", *bins_count), "cut6"))
+
+    ########################
+    # Final histograms
+    ########################
+    results.append(df.Histo1D(("higgs_m", "", *bins_m_ll), "higgs_m"))
+    results.append(df.Histo1D(("higgs_recoil_m", "", *bins_recoil), "higgs_recoil_m"))
+    results.append(df.Histo1D(("higgs_p", "", *bins_p_ll), "higgs_p"))
+    results.append(df.Histo1D(("higgs_muons_p", "", *bins_p_mu), "higgs_muons_p"))
+
+    return results, weightsum
+
+##### Guarda los resultados en archivos .root de salida
+#def save_to_output(process_name, output_file):
+ #   chain = load_files(process_name)
+  #  df = ROOT.RDataFrame(chain)
+    
+   #### Aplica tus filtros, selecciones, y cálculos sobre el DataFrame 'df'
+  #  df_processed = df.Filter("your_condition").Define("new_variable", "some_expression")
+    
+    ##### Guarda los resultados en un archivo .root de salida
+    #df_processed.Snapshot("treeName", output_file)
+
+# Ejemplo de uso
+#save_to_output('p8_ee_WW_mumu_ecm240', "output_WW_mumu.root")
+#save_to_output('p8_ee_ZH_Zmumu_ecm240', "output_ZH_Zmumu.root")
