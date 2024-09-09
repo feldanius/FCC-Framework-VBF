e08c85e277855f26c8b11477177572ff5eb9bd15
feldanius <43442037+feldanius@users.noreply.github.com>
2024-09-09 03:15:11 -0600

    Update histmaker_recoil_1.py

--- histmaker_recoil_1.py
+++ histmaker_recoil_1.py
@@ -61,7 +61,7 @@ procDict = "FCCee_procDict_winter2023_IDEA.json"
 includePaths = ["functions_1.h"]
 
 # Define the input dir (optional)
-inputDir = "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/"
+inputDir = "/afs/cern.ch/user/f/fdmartin/FCCAnalyses/tutorial"
 
 #Optional: output directory, default is local running directory
 outputDir = "./outputs/histmaker_1/recoil_1/"
