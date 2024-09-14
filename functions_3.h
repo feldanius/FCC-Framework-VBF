26f34f77ccfcc642f5f37902a7b0992ab844da14
feldanius <43442037+feldanius@users.noreply.github.com>
2024-09-14 12:57:35 -0600

    Update functions_3.h

--- functions_3.h
+++ functions_3.h
@@ -110,7 +110,7 @@ Vec_rp HiggsResonanceBuilder::HiggsResonanceBuilder::operator()(Vec_rp legs, Vec
             }
         }
 
-        if (idx_min >= 0 && idx_min < result.size()) {   //modification original:  if(idx_min > -1) 
+        if(idx_min > -1) {   
             bestReso.push_back(result.at(idx_min));
             auto & l1 = legs[pairs[idx_min][0]];
             auto & l2 = legs[pairs[idx_min][1]];
@@ -130,40 +130,127 @@ Vec_rp HiggsResonanceBuilder::HiggsResonanceBuilder::operator()(Vec_rp legs, Vec
     }
 }
 
-////////////////////////continue here///////////////////////77
+struct sel_iso {
+    sel_iso(float arg_max_iso);
+    float m_max_iso = .25;
+    Vec_rp operator() (Vec_rp in, Vec_f iso);
+  };
 
-struct JetMass {
-    Vec_rp operator() (Vec_rp in, float m_jet_mass);
-};
-
-Vec_rp JetMass::operator() (Vec_rp in, float m_jet_mass) {
+sel_iso::sel_iso(float arg_max_iso) : m_max_iso(arg_max_iso) {};
+ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData>  sel_iso::operator() (Vec_rp in, Vec_f iso) {
     Vec_rp result;
     result.reserve(in.size());
-    for (auto &p : in) {
-        if (std::fabs(p.mass - m_jet_mass) < 5.0) {
-            result.push_back(p);
+    for (size_t i = 0; i < in.size(); ++i) {
+        auto & p = in[i];
+        if (iso[i] < m_max_iso) {
+            result.emplace_back(p);
         }
     }
     return result;
 }
 
-struct DeltaPhi {
-    std::vector<float> operator() (Vec_rp in);
+ 
+// compute the cone isolation for reco particles
+struct coneIsolation {
+
+    coneIsolation(float arg_dr_min, float arg_dr_max);
+    double deltaR(double eta1, double phi1, double eta2, double phi2) { return TMath::Sqrt(TMath::Power(eta1-eta2, 2) + (TMath::Power(phi1-phi2, 2))); };
+
+    float dr_min = 0;
+    float dr_max = 0.4;
+    Vec_f operator() (Vec_rp in, Vec_rp rps) ;
 };
 
-std::vector<float> DeltaPhi::operator() (Vec_rp in) {
-    std::vector<float> result;
-    if (in.size() >= 2) {
-        TLorentzVector lv1;
-        lv1.SetPxPyPzE(in[0].momentum.x, in[0].momentum.y, in[0].momentum.z, in[0].energy);
-        TLorentzVector lv2;
-        lv2.SetPxPyPzE(in[1].momentum.x, in[1].momentum.y, in[1].momentum.z, in[1].energy);
-        double dphi = std::fabs(lv1.Phi() - lv2.Phi());
-        if (dphi > M_PI) dphi = 2 * M_PI - dphi;
-        result.push_back(dphi);
+coneIsolation::coneIsolation(float arg_dr_min, float arg_dr_max) : dr_min(arg_dr_min), dr_max( arg_dr_max ) { };
+Vec_f coneIsolation::coneIsolation::operator() (Vec_rp in, Vec_rp rps) {
+  
+    Vec_f result;
+    result.reserve(in.size());
+
+    std::vector<ROOT::Math::PxPyPzEVector> lv_reco;
+    std::vector<ROOT::Math::PxPyPzEVector> lv_charged;
+    std::vector<ROOT::Math::PxPyPzEVector> lv_neutral;
+
+    for(size_t i = 0; i < rps.size(); ++i) {
+
+        ROOT::Math::PxPyPzEVector tlv;
+        tlv.SetPxPyPzE(rps.at(i).momentum.x, rps.at(i).momentum.y, rps.at(i).momentum.z, rps.at(i).energy);
+        
+        if(rps.at(i).charge == 0) lv_neutral.push_back(tlv);
+        else lv_charged.push_back(tlv);
+    }
+    
+    for(size_t i = 0; i < in.size(); ++i) {
+
+        ROOT::Math::PxPyPzEVector tlv;
+        tlv.SetPxPyPzE(in.at(i).momentum.x, in.at(i).momentum.y, in.at(i).momentum.z, in.at(i).energy);
+        lv_reco.push_back(tlv);
+    }
+
+    
+    // compute the isolation (see https://github.com/delphes/delphes/blob/master/modules/Isolation.cc#L154) 
+    for (auto & lv_reco_ : lv_reco) {
+    
+        double sumNeutral = 0.0;
+        double sumCharged = 0.0;
+    
+        // charged
+        for (auto & lv_charged_ : lv_charged) {
+    
+            double dr = coneIsolation::deltaR(lv_reco_.Eta(), lv_reco_.Phi(), lv_charged_.Eta(), lv_charged_.Phi());
+            if(dr > dr_min && dr < dr_max) sumCharged += lv_charged_.P();
+        }
+        
+        // neutral
+        for (auto & lv_neutral_ : lv_neutral) {
+    
+            double dr = coneIsolation::deltaR(lv_reco_.Eta(), lv_reco_.Phi(), lv_neutral_.Eta(), lv_neutral_.Phi());
+            if(dr > dr_min && dr < dr_max) sumNeutral += lv_neutral_.P();
+        }
+        
+        double sum = sumCharged + sumNeutral;
+        double ratio= sum / lv_reco_.P();
+        result.emplace_back(ratio);
     }
     return result;
 }
+ 
+ 
+ 
+// returns missing energy vector, based on reco particles
+Vec_rp missingEnergy(float ecm, Vec_rp in, float p_cutoff = 0.0) {
+    float px = 0, py = 0, pz = 0, e = 0;
+    for(auto &p : in) {
+        if (std::sqrt(p.momentum.x * p.momentum.x + p.momentum.y*p.momentum.y) < p_cutoff) continue;
+        px += -p.momentum.x;
+        py += -p.momentum.y;
+        pz += -p.momentum.z;
+        e += p.energy;
+    }
+    
+    Vec_rp ret;
+    rp res;
+    res.momentum.x = px;
+    res.momentum.y = py;
+    res.momentum.z = pz;
+    res.energy = ecm-e;
+    ret.emplace_back(res);
+    return ret;
+}
+
+// calculate the cosine(theta) of the missing energy vector
+float get_cosTheta_miss(Vec_rp met){
+    
+    float costheta = 0.;
+    if(met.size() > 0) {
+        
+        TLorentzVector lv_met;
+        lv_met.SetPxPyPzE(met[0].momentum.x, met[0].momentum.y, met[0].momentum.z, met[0].energy);
+        costheta = fabs(std::cos(lv_met.Theta()));
+    }
+    return costheta;
+}
+
 
 } } // namespace FCCAnalyses::HiggsAnalysis
 
