3379fd1e891d2d6c3c5e5a734c7adb92b749627c
feldanius <43442037+feldanius@users.noreply.github.com>
2024-09-29 17:24:25 -0600

    Update histmaker_flavor_cut_missing_p-15.py

--- histmaker_flavor_cut_missing_p-15.py
+++ histmaker_flavor_cut_missing_p-15.py
@@ -65,7 +65,7 @@ def build_graph(df, dataset):
     ### CUT : cut jj_m
     #########
     df = df.Filter("jj_m > 95 && jj_m < 155")
-    df = df.Filter("missing_p > 15")
+    df = df.Filter("missing_p > 15 && missing_p < 170")
     df = df.Filter("scoresum_B > 1.0")
 
     #results.append(df.Histo1D(("higgs_recoil_m_mu", "", *bins_recoil), "higgs_recoil_m_mu"))
